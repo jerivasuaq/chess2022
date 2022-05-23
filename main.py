d737fe8820a7b125e0d301c0b1741c28b011292c
erikrivas-qro <erik.rivas@cornershopapp.com>
2022-05-23 17:51:23 -0500

    Added Piece and Pawn class, printed a board with pawns

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
-class Pawn:
+from abc import abstractclassmethod, abstractmethod
+
+
+class Piece:
     color = "b"
     i = 0
     j = 0
+    char = "X"
 
     def __init__(self, i, j) -> None:
-        pass
+        self.i = i
+        self.j = j
+
+    @abstractclassmethod
+    def move(self, i, j):
+        raise NotImplementedError
+
+    @abstractclassmethod
+    def attack(self, i, j):
+        raise NotImplementedError
+
+    def print(
+        self,
+    ):
+        print(self.char, end="")
+
+
+class Pawn(Piece):
+    color = "b"
+    char = "♟️"
+
+    def __init__(self, i, j) -> None:
+        super().__init__(i, j)
 
     def move(i, j):
         pass
@@ -24,18 +50,21 @@ if __name__ == "__main__":
         "pppppppp",
         "        ",
     ]
-    board = [[None] * 8] * 8
+    board = [[None] * 8 for _ in range(8)]
 
     # filling the board
     for i in range(len(s)):
-        for j in range(len(s)):
-            if s[i][j] == "p":
-                p = Peon(i, j)
-                board[i][j] = p
+        for j in range(len(s[0])):
+            if s[i][j] is not None:
+                if s[i][j] == 'p':
+                    p = Pawn(i, j)
+                    board[i][j] = p
+
 
     # printing the board
-    for i in range(len(S)):
-        for j in range(len(s)):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
             p = board[i][j]
-            if p:
+            if p is not None:
                 p.print()
+        print()
